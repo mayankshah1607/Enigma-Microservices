eabc1ae4f5a414f8e4ec0ba769d5760729417f18
mayankshah1607 <mayankshah1614@gmail.com>
2019-06-12 18:27:08 +0530

    Initial Commit
    Signed-off-by: mayankshah1607 <mayankshah1614@gmail.com>

---
+++ admin/pkg/service/service.go
@@ -0,0 +1,7 @@
+package service
+
+// AdminService describes the service.
+type AdminService interface {
+	// Add your methods here
+	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
+}
---
+++ auth/pkg/service/service.go
@@ -0,0 +1,7 @@
+package service
+
+// AuthService describes the service.
+type AuthService interface {
+	// Add your methods here
+	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
+}
