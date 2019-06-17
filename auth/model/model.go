5335b6495252563bb2e1d7dc230e832d36771841
mayankshah1607 <mayankshah1614@gmail.com>
2019-06-17 13:44:23 +0530

    Added model init

---
+++ auth/model/model.go
@@ -0,0 +1,36 @@
+package model
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+var db *mongo.Database
+var dbURI string
+
+func init() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env found!")
+	}
+	dbURI, _ = os.LookupEnv("DB_URI")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(dbURI))
+
+	if err != nil {
+		log.Println("Error connecting to database :", err.Error())
+		return
+	}
+
+	db = client.Database("enigma")
+
+}
